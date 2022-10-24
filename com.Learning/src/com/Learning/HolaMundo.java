258b434d7c02a6b264d4921e0c226a4469b2443a
CarreraMiguel <116556751+CarreraMiguel@users.noreply.github.com>
2022-10-24 12:40:11 +0200

    Add files via upload

---
+++ com.Learning/src/com/Learning/HolaMundo.java
@@ -0,0 +1,12 @@
+package com.Learning;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        byte dato1 = 1;
+        short edad = 25;
+        int dato2 = 500;
+        long dato3 = 50000;
+        String nombre = "Miguel";
+        System.out.println(" Hola open Bootcamp, mi nombre es " + nombre + " y tengo " + edad + " años.");
+    }
+}
