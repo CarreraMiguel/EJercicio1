f6d0ae7cde2b93ef9d79701aa80f64a460c2a83b
CarreraMiguel <116556751+CarreraMiguel@users.noreply.github.com>
2022-10-29 12:37:45 +0200

    Add files via upload

---
+++ com.Learning/src/com/Learning/Concatenar.java
@@ -0,0 +1,8 @@
+public class Concatenar {
+    public static void main(String[] args) {
+        String[] textos = {"Habia una vez una princesa", " que comía muchas manzanas" , " y por comer tantas se murió."};
+        for (String frase: textos){
+            System.out.print(frase);
+        }
+    }
+}
